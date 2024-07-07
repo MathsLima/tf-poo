59c41d10e038f410e1f0b6c026ee376074dfb2c5
garssalucas <garssa.lucas@gmail.com>
2024-07-07 10:34:44 -0300

    Criação classe Veiculo

---
+++ Veiculo.java
@@ -0,0 +1,37 @@
+public abstract class Veiculo implements Comparable<Veiculo> {
+    private String placa;
+    private String modelo;
+    private double capacidadeCarga;
+
+    public Veiculo(String placa, String modelo, double capacidadeCarga) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.capacidadeCarga = capacidadeCarga;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public double getCapacidadeCarga() {
+        return capacidadeCarga;
+    }
+
+    @Override
+    public int compareTo(Veiculo outro) {
+        return this.placa.compareTo(outro.placa);
+    }
+
+    @Override
+    public String toString() {
+        return "Veiculo{" +
+                "placa='" + placa + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", capacidadeCarga=" + capacidadeCarga +
+                '}';
+    }
+}
